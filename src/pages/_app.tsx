41992ee25b55d1eafc4ac61f24b4292c6fec566e
WesleyR10 <wesleyribas2015@gmail.com>
2024-01-18 14:16:12 -0300

    Add link to logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { AppProps } from "next/app"
 import Image from "next/image"
+import Link from 'next/link'
 import { Handbag } from "phosphor-react"
 
 import { ProductsProvider } from '@/contexts/ProductsCartContext'
@@ -19,8 +20,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <ProductsProvider>
 
         <Header>
-          <Image src={logoImg} width={130} height={52} alt="" />
-
+          <Link href="/">
+            <Image src={logoImg} width={130} height={52} alt="" />
+          </Link>
           <Dialog.Root>
             <Dialog.Trigger asChild>
               <ShoppingCartButton> <Handbag size={24} /> <span>1</span> </ShoppingCartButton>
